d4000680faa5048eb4f7e1ed5879554c75a64242
PanzerWebDeveloper <selwynvillar@gmail.com>
2023-10-15 08:48:59 +0800

    Update

--- todoscript.js
+++ todoscript.js
@@ -1,37 +1,43 @@
+//todo | Define an array to store to-do items
 let todoItems = [];
 
-function renderTodo(todo){
+//todo |  Function to render a to-do item
+function renderTodo(todo) {
+    //todo |  Get the list element and the item element by data key
     const list = document.querySelector('.js-todo-list');
-    const item = document.querySelector(`[data-key = "${todo.id}"]`);
-    
+    const item = document.querySelector(`[data-key="${todo.id}"]`);
+
+    //todo |  If the to-do item is marked as deleted, remove it
     if (todo.deleted) {
         item.remove();
         if (todoItems.length === 0) list.innerHTML = '';
         return;
     }
 
+    //todo |  Format the date
     const { year, month, day, hour, minutes } = todo.date;
     const formattedDate = `${year}/${month}/${day}-${hour}:${minutes}`;
 
-    const isChecked = todo.checked ? 'done':'';
+    //todo |  Determine if the item is checked
+    const isChecked = todo.checked ? 'done' : '';
+    
+    //todo |  Create a new list item
     const node = document.createElement('li');
     node.setAttribute('class', `todo-item ${isChecked}`);
     node.setAttribute('data-key', todo.id);
-    node.innerHTML = 
-    `
-
-    <label for="${todo.id}" class="tick js-tick">
-    <input id="${todo.id}" type="checkbox"/ class="checkbox js-checkbox">   
-    </label>
-
-    <span>${todo.text}</span>
-    <button class="delete-todo js-delete-todo">
-        <svg><use href="#delete-icon">     <span id="date-span">${formattedDate}</span>
-
-        </use></svg>
+    node.innerHTML = `
+        <label for="${todo.id}" class="tick js-tick">
+            <input id="${todo.id}" type="checkbox" class="checkbox js-checkbox">
+        </label>
+        <span>${todo.text}</span>
+        <button class="delete-todo js-delete-todo">
+            <svg><use href="#delete-icon">
+            <span id="date-span">${formattedDate}</span>
+            </use></svg>
         </button>
     `;
 
+    //todo |  If the item already exists, replace it; otherwise, append it to the list
     if (item) {
         list.replaceChild(node, item);
     } else {
@@ -39,9 +45,11 @@ function renderTodo(todo){
     }
 }
 
-function addTodo(text){
+//todo |  Function to add a to-do item
+function addTodo(text) {
+    //*Create an object with text, checked: false, id: Date.now() properties
     const todoObj = {
-        text, 
+        text,
         checked: false,
         id: Date.now(),
         date: {
@@ -52,40 +60,49 @@ function addTodo(text){
             minutes: new Date().getMinutes(),
         }
     };
+    //* Push the object to empty array
     todoItems.push(todoObj);
+    //*Update local storage
     localStorage.setItem('todoItems', JSON.stringify(todoItems));
+
+    //* Call the rendering function
     renderTodo(todoObj);
 }
 
-function toggleDone(key){
+//todo |  Function to toggle the "done" status of a to-do item
+function toggleDone(key) {
     const index = todoItems.findIndex((item) => item.id === Number(key));
     todoItems[index].checked = !todoItems[index].checked;
     localStorage.setItem('todoItems', JSON.stringify(todoItems));
-    
     renderTodo(todoItems[index]);
 }
 
-function deleteTodo(key){
+//todo |  Function to delete a to-do item
+function deleteTodo(key) {
     const index = todoItems.findIndex((item) => item.id === Number(key));
-    const todo = { deleted: true, ...todoItems[index]};
+    const todo = { deleted: true, ...todoItems[index] };
 
     todoItems = todoItems.filter((item) => item.id !== Number(key));
     localStorage.setItem('todoItems', JSON.stringify(todoItems));
     renderTodo(todo);
 }
+
+//todo |  Get the form and set up a submit event listener
 const form = document.querySelector('.js-form');
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function(event) {
     event.preventDefault();
     const input = document.querySelector('.js-todo-input');
     const text = input.value.trim();
-    if(text != ''){
+    if (text !== '') {
         addTodo(text);
         input.value = '';
         input.focus();
     }
-})
+});
+
+//todo |  Get the list and set up a click event listener for item checkboxes and delete buttons
 const list = document.querySelector('.js-todo-list');
-list.addEventListener('click',  function(event){
+list.addEventListener('click', function(event) {
     if (event.target.classList.contains('js-tick')) {
         const itemKey = event.target.parentElement.dataset.key;
         toggleDone(itemKey);
@@ -94,15 +111,15 @@ list.addEventListener('click',  function(event){
         const itemKey = event.target.parentElement.dataset.key;
         deleteTodo(itemKey);
     }
-})
+});
 
-document.addEventListener('DOMContentLoaded', function(){
-    const ref = localStorage.getItem('todoItems');
-    if (ref) {
-        todoItems = JSON.parse(ref);
-        todoItems.forEach(function(save){
-            renderTodo(save);
-        })
+//todo |  When the page is loaded, check for saved to-do items in local storage and render them
+document.addEventListener('DOMContentLoaded', function() {
+    const storedItems = localStorage.getItem('todoItems');
+    if (storedItems) {
+        todoItems = JSON.parse(storedItems);
+        todoItems.forEach(function(savedItem) {
+            renderTodo(savedItem);
+        });
     }
-})
-
+});
